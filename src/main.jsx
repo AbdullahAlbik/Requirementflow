e4e645e7dbe1d5068b09cf54d4514eb27e3bdd67
AbdullahAlbik <abdullah.albik@web.de>
2026-04-10 00:50:31 +0200

    Add resilient boot fallback and broaden Pages deploy trigger

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
 
+window.__REQFLOW_BOOTED__ = true;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 )
